15d65595c5f1e70752b900e7d263902394c32b9e
Adhil Fouzi <adhilfouzi@gmail.com>
2024-04-27 23:33:57 +0530

    turf details screen added

---
+++ lib/view/course/turflist/screens/view_turf_details.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import '../../../../model/data_model/owner_model.dart';
+
+class ViewTurfDetailsScreen extends StatelessWidget {
+  final OwnerModel turf;
+  const ViewTurfDetailsScreen({Key? key, required this.turf}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios_rounded, color: Colors.black),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            // Image
+            Container(
+              height: 400,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: const Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.asset(
+                  "assets/image/turf_image.jpg",
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            // Turf name
+            Text(
+              turf.courtName,
+              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8),
+            // Address
+            Row(
+              children: [
+                const Icon(Icons.location_on, color: Color(0xFF238C98)),
+                const SizedBox(width: 8),
+                Text(
+                  turf.courtLocation,
+                  style: const TextStyle(fontSize: 16, color: Colors.grey),
+                ),
+              ],
+            ),
+            const SizedBox(height: 16),
+            // Description
+            Text(
+              turf.courtDescription,
+              style: const TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 24),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        elevation: 0,
+        color: Colors.transparent,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            // Call button
+            whiteButton('Call', context, () {
+              // _makePhoneCall(turf.courtPhoneNumber);
+            }),
+            // Book now button
+            mainButton('Book Now', context, () {
+              // Handle booking action
+            }),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // Function to make a phone call
+  void _makePhoneCall(String phoneNumber) async {
+    String url = 'tel:$phoneNumber';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  // Function to create main button with custom style
+  Widget mainButton(String text, BuildContext context, VoidCallback onPressed) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        foregroundColor: Colors.white,
+        backgroundColor: const Color(0xFF238C98), // Text color
+        minimumSize:
+            Size(MediaQuery.of(context).size.width * 0.4, 50), // Button width
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(999)), // Button corner radius
+      ),
+      child: Text(text),
+    );
+  }
+
+  // Function to create white button with custom style
+  Widget whiteButton(
+      String text, BuildContext context, VoidCallback onPressed) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        foregroundColor: const Color(0xFF238C98),
+        backgroundColor: Colors.white, // Text color
+        minimumSize:
+            Size(MediaQuery.of(context).size.width * 0.4, 50), // Button width
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(999), // Button corner radius
+          side:
+              BorderSide(color: const Color(0xFF238C98)), // Button border color
+        ),
+      ),
+      child: Text(text),
+    );
+  }
+}
--- lib/view/course/turflist/widget/turf_list_item.dart
+++ lib/view/course/turflist/widget/turf_list_item.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:users_side_of_turf_booking/view/course/turflist/screens/view_turf_details.dart';
 import '../../../../model/data_model/owner_model.dart';
 
 class TurfListItem extends StatelessWidget {
   final OwnerModel turf;
 
   const TurfListItem({
-    super.key,
+    Key? key,
     required this.turf,
-  });
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -26,24 +27,33 @@ class TurfListItem extends StatelessWidget {
             width: width * 0.25,
             height: height * 0.1,
             decoration: BoxDecoration(
-              color: Colors.blue,
               borderRadius: BorderRadius.circular(8),
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: AspectRatio(
+                aspectRatio: 16 / 9, // Adjust aspect ratio as needed
+                child: Image.asset(
+                  "assets/image/turf_image.jpg",
+                  fit: BoxFit.cover,
+                ),
+              ),
             ),
           ),
           title: Text(
-            turf.courtName, // Use turf name from OwnerModel
+            turf.courtName,
             style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
           ),
           subtitle: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
-                turf.courtLocation, // Use turf location from OwnerModel
+                turf.courtLocation,
                 style: TextStyle(color: Colors.grey[600], fontSize: 14),
               ),
               const SizedBox(height: 4),
               Text(
-                '${turf.openingTime} to ${turf.closingTime}', // Use turf distance from OwnerModel
+                '${turf.openingTime} to ${turf.closingTime}',
                 style: TextStyle(
                   fontWeight: FontWeight.bold,
                   color: Colors.grey[600],
@@ -52,7 +62,8 @@ class TurfListItem extends StatelessWidget {
             ],
           ),
           onTap: () {
-            // Add your onTap action here
+            Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => ViewTurfDetailsScreen(turf: turf)));
           },
         ),
       ),
