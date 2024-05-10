ded9d3c169af86312fe7e9e15aba4905426591aa
Adhil Fouzi <adhilfouzi@gmail.com>
2024-05-10 14:59:14 +0530

    canceled and completed turf list designed

--- lib/view/course/mybooking/screens/show_booking/canceled_booking.dart
+++ lib/view/course/mybooking/screens/show_booking/canceled_booking.dart
@@ -1,15 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+
+import 'active_booking.dart';
 
 class CanceledBooking extends StatelessWidget {
   const CanceledBooking({super.key});
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.yellow,
-      child: Center(
-        child: Text('Canceled Booking'),
-      ),
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    return ListView.builder(
+      itemCount: 10,
+      itemBuilder: (BuildContext context, int index) {
+        return Card(
+          elevation: 4,
+          margin: EdgeInsets.symmetric(
+            vertical: height * 0.01,
+            horizontal: width * 0.02,
+          ),
+          child: Padding(
+            padding: EdgeInsets.all(width * 0.02),
+            child: Column(
+              children: [
+                BookingDetails(),
+                SizedBox(height: height * 0.02),
+                const Text(
+                  "Canceled this turf booking",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 15,
+                    color: Color.fromARGB(221, 255, 0, 0),
+                  ),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                SizedBox(height: height * 0.02),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
--- lib/view/course/mybooking/screens/show_booking/completed_booking.dart
+++ lib/view/course/mybooking/screens/show_booking/completed_booking.dart
@@ -1,12 +1,38 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../../utils/portion/button.dart';
+import 'active_booking.dart';
 
 class CompletedBooking extends StatelessWidget {
   const CompletedBooking({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Completed Booking'),
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    return ListView.builder(
+      itemCount: 10,
+      itemBuilder: (BuildContext context, int index) {
+        return Card(
+          elevation: 4,
+          margin: EdgeInsets.symmetric(
+            vertical: height * 0.01,
+            horizontal: width * 0.02,
+          ),
+          child: Padding(
+            padding: EdgeInsets.all(width * 0.02),
+            child: Column(
+              children: [
+                BookingDetails(),
+                SizedBox(height: height * 0.01),
+                Button().mainButton("Booking Completed", context, () {
+                  // Add action for button press
+                }),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
